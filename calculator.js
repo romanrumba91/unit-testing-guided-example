abdf3b5596d7a62d448e7c63582e37b9e8f1ede7
Roman <romandiazloz91@gmail.com>
2022-01-11 11:16:01 -0600

    Suma Realizada

--- calculator.js
+++ calculator.js
@@ -1,13 +1,62 @@
+console.log("hola mundo")
+
+// FUNCIONES DECLARATIVAS
+// SOLO SE HACE UNA VEZ
+// function
 function sum(a, b) {
-  return;
+
+  // EN CASO DE QUE NO HAYA ARGUMENTOS
+  if (!a && !b) {
+    return 0
+  }
+
+  // EN CASO DE QUE EL SEGUNDO ARGUMENTO NO EXISTA
+  if(!b) {
+    b = 0
+  }
+
+  const total = a + b  
+
+  return total;
 }
 
+// INVOCACIÓN DE FUNCIÓN
+// PUEDE USARASE (INVOCARSE) LAS VECES QUE QUIERAN
+ console.log(sum())
+ console.log(sum(5))
+
 function subtract(a, b) {
-  return;
+  
+  // EN CASO DE QUE NO HAYA ARGUMENTOS
+  if (!a && !b) {
+    return 0
+  }
+
+  // EN CASO DE QUE EL SEGUNDO ARGUMENTO NO EXISTA
+  if(!b) {
+    b = 0
+  }
+
+  const totalSub = a - b
+
+  return totalSub;
 }
 
 function divide(a, b) {
-  return;
+
+    // EN CASO DE QUE NO HAYA ARGUMENTOS
+    if (a===0 && b===0) {
+      return error
+    }
+  
+    // EN CASO DE QUE EL SEGUNDO ARGUMENTO NO EXISTA
+    if(!b) {
+      b = 0
+    }
+
+    const totalDiv = a / b
+
+  return totalDiv;
 }
 
 function multiply(a, b) {
